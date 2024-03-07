94373ef1903a88e02c324b6951e1f98487977b4e
Jeevitha3105 <138574554+Jeevitha3105@users.noreply.github.com>
2024-03-07 22:23:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,12 +12,20 @@ import {profileRouter} from './routes/Profile.js'
 const app = express()
 
 app.use(morgan("dev"));
+// app.use(cors(
+//     {
+//         origin:["http://localhost:5173"],
+//         credentials: true
+//     }
+// ));
+
 app.use(cors(
     {
-        origin:["http://localhost:5173"],
+        origin:["https://e-biz-card-backend.vercel.app"],
         credentials: true
     }
 ));
+
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 
